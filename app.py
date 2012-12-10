ee0b0d136b061acf733499a3d8aef937ad14987c
Eric Pierce <wapcaplet88@gmail.com>
2012-12-09 21:23:21 -0700

    Use PORT from environment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -20,6 +21,7 @@ def multiples():
     return render_template('multiples.html')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    #app.debug = True
+    app.run(host='0.0.0.0', port=port)
 
